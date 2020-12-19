82107182119a5b2457e8a18a96ca9567057ba395
Карлаш Лариса Станиславовна <larisa.karlash@metinvestholding.com>
2020-12-20 00:09:37 +0200

    Добавлена папка lesson_3

---
+++ lesson_3/lesson_3/Controllers/NewsController.cs
@@ -0,0 +1,17 @@
+﻿using lesson_3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lesson_3.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ lesson_3/lesson_3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace lesson_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
