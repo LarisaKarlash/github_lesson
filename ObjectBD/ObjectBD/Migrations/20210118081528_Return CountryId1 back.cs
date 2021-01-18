c9bc44dba99a68efa0064ace39c8a3fcc0c432d9
Карлаш Лариса Станиславовна <larisa.karlash@metinvestholding.com>
2021-01-18 10:21:22 +0200

    CountryId1 вернула

---
+++ ObjectBD/ObjectBD/Migrations/20210118081528_Return CountryId1 back.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ObjectBD.Migrations
+{
+    public partial class ReturnCountryId1back : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                        name: "FK_Humans_Countries_CountryId",
+                        table: "Humans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Humans_CountryId",
+                table: "Humans");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CountryId1",
+                table: "Humans",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Humans_CountryId1",
+                table: "Humans",
+                column: "CountryId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Humans_Countries_CountryId1",
+                table: "Humans",
+                column: "CountryId1",
+                principalTable: "Countries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
