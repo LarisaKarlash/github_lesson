dc10212df52d534f76a5d3b5f2c9900a02bdd4eb
Карлаш Лариса Станиславовна <larisa.karlash@metinvestholding.com>
2021-01-26 16:23:20 +0200

    добавлено классное задание № 11

---
+++ RestExz/RestExz/Controllers/FileController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestExz.Controllers
+{
+    public class FileController
+    {
+        [HttpGet("File")]
+        public FileContentResult Get()
+        {
+            var fileBytes = System.IO.File.ReadAllBytes("wwwroot/TerrainImage55.jpg");
+            return new FileContentResult(fileBytes, "image/jpeg");
+        }
+
+        [HttpPost("File")]
+        public void UploadFile([FromBody] string file, [FromQuery] string fileName, [FromServices] IWebHostEnvironment webHost)
+        {
+            var filePath = Path.Combine(webHost.WebRootPath, fileName);
+            var fileContent = Convert.FromBase64String(file);
+            System.IO.File.WriteAllBytes(filePath, fileContent);
+        }
+    }
+}
